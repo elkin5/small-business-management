721c06d0566887effa0972190494337c18e983ec
Elkin Hurtado <syselkin5@gmail.com>
2023-08-07 13:43:18 -0500

    Inicio de proyecto

---
+++ src/main/kotlin/com/co/kinsoft/apps/small_business_management/SmallBusinessManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.co.kinsoft.apps.small_business_management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmallBusinessManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmallBusinessManagementApplication>(*args)
+}
---
+++ src/test/kotlin/com/co/kinsoft/apps/small_business_management/SmallBusinessManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.co.kinsoft.apps.small_business_management
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmallBusinessManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
